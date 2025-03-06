1f0203ea75c16e7c602ac576abd53da4cfda07a5
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2025-03-06 14:41:20 +0530

    Count of character in a string

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import javax.swing.plaf.IconUIResource;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        String word = "Learn Test Automation with Java";
+        /*String word = "Learn Test Automation with Java";
 
         String rev = "";
 
@@ -13,6 +15,20 @@ public class Main {
         for(int i= splitword.length-1; i>=0; i--){
             rev = rev + splitword[i] + " ";
         }
-        System.out.println(rev);
+        System.out.println(rev);*/
+        String input = "LearnAutomation";
+        char toFind = 'a'; // Correct way to assign a character
+        int Count = 0;
+
+        input = input.toLowerCase();
+
+        for(int i =0; i<input.length();i++){
+            if(input.charAt(i) == toFind){
+                Count = Count + 1;
+            }
+        }
+
+        System.out.println(toFind + "is present" + Count + "Number of times");
+
     }
-}
+    }
